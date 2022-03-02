0262cee2ceb5ebf1e1baa101bc69670cdb3744b4
Eron-Home <eroncmc@gmail.com>
2022-03-02 10:13:54 -0300

    Comentario Versão 1.1

--- app/src/main/java/com/example/olamundo/MainActivity.kt
+++ app/src/main/java/com/example/olamundo/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.olamundo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// Comentário Versão 1.1
 class MainActivity : AppCompatActivity() {
 
-    //TESTE 1
+    //TESTE 1 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
